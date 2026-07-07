2dabb34a5a2498d1383c8a6a246d9df1d2de26bb
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-06 20:10:57 -0600

    refactor(types): segregate global types

--- src/components/CityWeather.tsx
+++ src/components/CityWeather.tsx
@@ -3,6 +3,7 @@ import ImageFixed from "next/image"
 
 import { upperCaseFirstLetterOfEachWord } from "@/src/utils/text"
 import { KtoF } from "@/src/utils/weather"
+import { CurrentWeatherData } from "@/src/types/weather"
 
 import Card from "@/src/components/Card"
 import Temperature from "@/src/components/Temperature"
