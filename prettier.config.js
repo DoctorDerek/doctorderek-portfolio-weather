eebe9360b12917d29339d9d7f907d38c5325e088
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:33:02 -0600

    chore(config): simplify prettier config to strict MQA spec

--- prettier.config.js
+++ prettier.config.js
@@ -1,28 +1,5 @@
-/** @type {import('prettier').Config} */
 module.exports = {
   semi: false,
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
-  importOrder: [
-    "^(react/(.*)$)|^(react$)",
-    "^(next/(.*)$)|^(next$)",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "^types$",
-    "^@/src/types/(.*)$",
-    "^@/src/config/(.*)$",
-    "^@/src/lib/(.*)$",
-    "^@/src/hooks/(.*)$",
-    "^@/src/components/ui/(.*)$",
-    "^@/src/components/(.*)$",
-    "^@/src/registry/(.*)$",
-    "^@/src/styles/(.*)$",
-    "^@/src/app/(.*)$",
-    "",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "5.0.0",
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  importOrder: ["^@/(.*)$", "^[./]"],
 }
