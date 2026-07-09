4ee6b7ab39a249a20964c2d1de196ff74b77adca
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 18:48:27 -0600

    fix(deps): downgrade typescript to v6 for eslint compatibility

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,18 @@
 import gitignore from "eslint-config-flat-gitignore"
 import nextVitals from "eslint-config-next/core-web-vitals"
-// import nextTs from "eslint-config-next/typescript"
-import prettier from "eslint-config-prettier/flat"
-import { defineConfig } from "eslint/config"
+import nextTs from "eslint-config-next/typescript"
+import prettier from "eslint-config-prettier"
 import "eslint-plugin-only-warn"
 
-const eslintConfig = defineConfig([
+export default [
   gitignore(),
   ...nextVitals,
   /**
    * ONE-TIME EXCEPTION TO NO CODE COMMENT RULE:
    * typescript-eslint (v8.63.0) is broken with TypeScript 7 (v7.0.2)
-   * TODO Restore typescript-eslint rules as soon as possible!!
+   * until TS 7 releases an API (planned for v7.1.0+)
+   * TODO Upgrade to TS 7 when the version is >7.1.0 and typescript-eslint is working with TS7
    * */
-  // ...nextTs,
+  ...nextTs,
   prettier,
-])
-
-export default eslintConfig
+]
