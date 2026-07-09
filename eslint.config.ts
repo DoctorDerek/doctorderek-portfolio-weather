32dacbf8bc0a924bd6dabaefa588f4ebb3a4c7ac
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 16:04:27 -0600

    chore(config): explicitly type eslint.config.ts as Linter.Config[]

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,12 +4,13 @@
  * until TS 7 releases an API (planned for v7.1.0+)
  * TODO Upgrade to TS 7 when the version is >7.1.0 and typescript-eslint is working with TS7
  * */
+import type { Linter } from "eslint"
 import gitignore from "eslint-config-flat-gitignore"
 import nextConfig from "eslint-config-next"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 import onlyWarn from "eslint-plugin-only-warn"
 
-const eslintConfig = [
+const eslintConfig: Linter.Config[] = [
   gitignore(),
   ...nextConfig,
   eslintConfigPrettier,
