d350f2a611fd420494a2f4322513aaaf554eefef
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 16:31:21 -0600

    style(formatting): run Prettier code formatter

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,7 @@
  * until TS 7 releases an API (planned for v7.1.0+)
  * TODO Upgrade to TS 7 when the version is >7.1.0 and typescript-eslint is working with TS7
  * */
-import type { Linter, ESLint } from "eslint"
+import type { ESLint, Linter } from "eslint"
 import gitignore from "eslint-config-flat-gitignore"
 import nextConfig from "eslint-config-next"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
