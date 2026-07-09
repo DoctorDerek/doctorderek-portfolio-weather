a4a9df4a08917838de4f78f423cf2ea967df7b4c
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 22:06:39 -0600

    refactor(config): match ESLint config with DoctorDerek.com pattern

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,23 @@
-import nextVitals from "eslint-config-next/core-web-vitals"
-// import nextTs from "eslint-config-next/typescript"
-import prettier from "eslint-config-prettier/flat"
-import { defineConfig, globalIgnores } from "eslint/config"
-import "eslint-plugin-only-warn"
+/**
+ * ONE-TIME EXCEPTION TO NO CODE COMMENT RULE:
+ * typescript-eslint (v8.63.0) is broken with TypeScript 7 (v7.0.2)
+ * until TS 7 releases an API (planned for v7.1.0+)
+ * TODO Upgrade to TS 7 when the version is >7.1.0 and typescript-eslint is working with TS7
+ * */
+import gitignore from "eslint-config-flat-gitignore"
+import nextConfig from "eslint-config-next"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import onlyWarn from "eslint-plugin-only-warn"
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  /**
-   * ONE-TIME EXCEPTION TO NO CODE COMMENT RULE:
-   * typescript-eslint (v8.63.0) is broken with TypeScript 7 (v7.0.2)
-   * until TS 7 releases an API (planned for v7.1.0+)
-   * TODO Upgrade to TS 7 when the version is >7.1.0 and typescript-eslint is working with TS7
-   * */
-  // ...nextTs,
-  prettier,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-])
+const eslintConfig = [
+  gitignore(),
+  ...nextConfig,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "only-warn": onlyWarn,
+    },
+  },
+]
 
 export default eslintConfig
